612e1a51f8af11e4245f04a1ae9bc16e1199d2c8
Doğukan Arkan <dogukan.arkan@bordatech.com>
2019-07-03 22:24:33 +0300

    Check methods and CreatePersonDeck method added.

--- BordaGame/Game.cs
+++ BordaGame/Game.cs
@@ -10,6 +10,9 @@ namespace BordaGame
 {
     public class Game
     {
+        private const int CHARACTERCOUNT = 4;
+        private Person _person = new Person();
+        private Computer _computer = new Computer();
         private List<string> _characterList = AppDomain.CurrentDomain
                             .GetAssemblies()
                             .SelectMany(x => x.GetTypes())
@@ -96,6 +99,33 @@ namespace BordaGame
             {
                 Console.WriteLine($"{count++}. {item.ToString()}");
             }
+        }
+
+        public void CreatePersonDeck(int level)
+        {
+            do
+            {
+                string input = Console.ReadLine();
+                if (CheckCharacterName(input) && !CheckDeck(input))
+                {
+                    _person.Deck.Add(CharacterFactory.Build(input));
+                    Console.WriteLine($"{input} added successfully.");
+                }
+                else
+                {
+                    Console.WriteLine("Please enter the character name again.");
+                }
+            } while (_person.Deck.Count < (CHARACTERCOUNT + level));
+        }
+
+        private bool CheckCharacterName(string name)
+        {
+            return _characterList.Any(s => name.Contains(s));
+        }
+
+        public bool CheckDeck(string name)
+        {
+            return _person.Deck.Any(s => name.Contains(s.GetType().Name));
         }
     }
 }
