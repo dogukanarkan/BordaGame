9df898a7a6c62b5c37a341db4f04dcb417687d39
Doğukan Arkan <dogukan.arkan@bordatech.com>
2019-07-03 22:04:38 +0300

    Player, Person and Computer classes created.

---
+++ BordaGame/Person.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BordaGame
+{
+    public class Person : Player
+    {
+        public Person() : base() { }
+    }
+}
---
+++ BordaGame/Player.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BordaGame
+{
+    public abstract class Player
+    {
+        public Player()
+        {
+            Threshold = 100;
+            Level = 1;
+            Experience = 0;
+            Deck = new List<ICharacter>();
+            Character = null;
+        }
+
+        public int Threshold { get; set; }
+        public int Level { get; set; }
+        public int Experience { get; set; }
+        public List<ICharacter> Deck { get; set; }
+        public ICharacter Character { get; set; }
+    }
+}
