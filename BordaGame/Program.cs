c9497f0febbc67258268d234bf44932a7c992162
Doğukan Arkan <dogukan.arkan@bordatech.com>
2019-07-04 13:34:24 +0300

    Game initialization method called in Program class.

--- BordaGame/Program.cs
+++ BordaGame/Program.cs
@@ -2,11 +2,11 @@
 
 namespace BordaGame
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            new Game().InitGame();
         }
     }
 }
